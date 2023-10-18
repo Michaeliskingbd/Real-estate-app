86aa75873d435944175a41fec0bf948b0fc180f8
michaelAladejuyigbe <michaelaladejuyigbe@gmail.com>
2023-10-18 16:59:21 +0100

    first commit

--- src/pages/createpage/CreatePage.jsx
+++ src/pages/createpage/CreatePage.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./createpage.css"
 import { Link } from 'react-router-dom'
 
 const CreatePage = () => {
+
+    const [count, setCount] = useState(0)
+    const [counttwo, setCounttwo] = useState(0)
+
+    const Increase = () => {
+
+            setCount(count +1)  
+    }
+
+    const Decrease = () => {
+        if (
+            count > 0 
+        ) {
+            setCount(count -1)
+        }
+       
+    }
+
+
+    const Increasetwo = () => {
+
+            setCounttwo(counttwo +1)  
+    }
+
+    const Decreasetwo = () => {
+        if (
+            count > 0 
+        ) {
+            setCounttwo(counttwo -1)
+        }
+       
+    }
+
   return (
     <div className='secondary__container'>
         
@@ -24,8 +57,8 @@ const CreatePage = () => {
 
                 <div className='bnb'>
                 <label>Beds and Bathrooms</label>
-                <div><span>Number of bedrooms</span>  <div className='count__box'><i class="fa-solid fa-minus"></i> <p> 4</p> <i class="fa-solid fa-plus"></i></div> </div>
-                <div><span>Number of bathrooms</span>   <div className='count__box'><i class="fa-solid fa-minus"></i> <p> 4</p> <i class="fa-solid fa-plus"></i></div></div>
+                <div><span>Number of bedrooms</span>  <div className='count__box'><i onClick={Decrease} class="fa-solid fa-minus"></i> <p>{count}</p> <i onClick={Increase} class="fa-solid fa-plus"></i></div> </div>
+                <div><span>Number of bathrooms</span>   <div className='count__box'><i  onClick={Decreasetwo} class="fa-solid fa-minus"></i> <p>{counttwo}</p> <i  onClick={Increasetwo} class="fa-solid fa-plus"></i></div></div>
                 </div>
                 
 
@@ -33,7 +66,7 @@ const CreatePage = () => {
 
                 <div className='create__location'>
                     <h2>Location</h2>
-                    <p>Where is thid property located?</p>
+                    <p>Where is this property located?</p>
                     <input className='create__input' type='text' placeholder='Location'/>
                 </div>
 
